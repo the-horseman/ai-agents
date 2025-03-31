e3b9ea925ffad64454bb88663b5749611ec83bb9
the-horseman07 <aryanrishi20@gmail.com>
2025-03-31 21:02:27 +0530

    masked credentials and added correct readme

--- app/utils/constants.py
+++ app/utils/constants.py
@@ -1,13 +1,13 @@
 INSIGHT_SCOPES = "ins.noti.r ins.suser ins.user"
-TENANT_USERNAME = "mvision.e2e+3USW03@gmail.com"
-TENANT_PASSWORD = "$y$teM73aM@9"
+TENANT_USERNAME = "< YOUR USERNAME HERE >"
+TENANT_PASSWORD = "< YOUR USER PASSWORD HERE >"
 IAM_TOKEN_URL = "https://iam.cloud.trellix.com/iam/v1.0/token"
 
 INSIGHTS_API_BASE_URL="https://insights.trellix.com/api/v1"
-HISTORICAL_API_BASE_URL = "https://sgw.soc.preprod.trellix.com/edrpreprod-search-service/searchservice/80/http/search-service/api/v1"
+HISTORICAL_API_BASE_URL = "< YOUR CENTRAL DB EXPOSED API HERE >"
 TENANT_ID = "3474F349-9405-49D0-A01C-284B80D48F88"
 #TENANT_ID = "9AC56A98-FD6D-4C4F-BD8B-3E58CAAC30BE"
-AUTH = "Basic dXNlcjpVczNyMjAyMA=="
+AUTH = "< YOUR AUTH HERE >"
 RTS_API_BASE_URL = "https://sgw.soc.preprod.trellix.com/edrpreprodmar/active-response/80/http/api/v1"
 
 DB_URI = "postgresql://postgres:postgres@localhost:5432/postgres?sslmode=disable"
